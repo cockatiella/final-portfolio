3067cb331131a435b59490176cfe82ab5ce87bea
Sofia Dinatolo <92044969+cockatiella@users.noreply.github.com>
2023-07-16 15:33:13 +0200

    fixed hamburger menu problem

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 let contactBtn = document.getElementById("contact-btn");
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+const navLinks = document.querySelectorAll('.nav-link');
 
-contactBtn.addEventListener("click", function () {
-  //go to contact-me section
-  let contactMe = document.getElementById("contact-me");
-  contactMe.scrollIntoView({ behavior: "smooth" });
+navbarToggler.addEventListener('click', function () {
+  navbarCollapse.classList.toggle('show');
 });
 
-function toggleDarkMode() {
-  var body = document.body;
-  var icon = document.querySelector(".btn-dark-mode i");
-  body.classList.toggle("dark-mode");
-  icon.classList.toggle("fa-moon");
-  icon.classList.toggle("fa-sun");
-}
+// Close the navigation bar when a link is clicked
+navLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
+    navbarCollapse.classList.remove('show');
+  });
+});
+
+// Close the navigation bar when clicked outside
+document.addEventListener('click', function (event) {
+  const targetElement = event.target;
+  if (
+    !targetElement.classList.contains('navbar-toggler') &&
+    !targetElement.closest('.navbar-collapse') &&
+    navbarCollapse.classList.contains('show')
+  ) {
+    navbarCollapse.classList.remove('show');
+  }
+
+  contactBtn.addEventListener("click", function () {
+    //go to contact-me section
+    let contactMe = document.getElementById("contact-me");
+    contactMe.scrollIntoView({ behavior: "smooth" });
+  });
+
+  function toggleDarkMode() {
+    var body = document.body;
+    var icon = document.querySelector(".btn-dark-mode i");
+    body.classList.toggle("dark-mode");
+    icon.classList.toggle("fa-moon");
+    icon.classList.toggle("fa-sun");
+  }
+})
